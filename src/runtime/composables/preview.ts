7c4314872d30f25e3808101022abfa6820d09eed
Matthew Rollinson <matt@rolley.io>
2025-05-20 14:37:37 +0100

    Visual editor page

--- src/runtime/composables/preview.ts
+++ src/runtime/composables/preview.ts
@@ -1,5 +1,3 @@
-import type { RouteLocationNormalized } from 'vue-router'
-
-export function isVisualEditorPage(route: RouteLocationNormalized): boolean {
-  return !!(route.query['visual-editor'] && route.query['visual-editor'] === 'true')
+export function isVisualEditorPage(route: any) {
+  return route.query['visual-editor'] === 'true' || route.query['visual-editor'] === true || route.query['visual-editor'] === 1
 }
