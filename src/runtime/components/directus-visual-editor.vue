0db3935858c8a59b2943defaaaa4c0979877d0e3
kheiner <heiner@heiner.work>
2025-11-18 10:18:33 -0800

    refactor: tabs v spaces
    I opened an IDE that had a config for indenting all elements of a script tag

--- src/runtime/components/directus-visual-editor.vue
+++ src/runtime/components/directus-visual-editor.vue
@@ -1,76 +1,76 @@
 <script setup lang="ts" generic="T extends keyof DirectusSchema">
-	import type { PrimaryKey } from '@directus/types'
-	import {
-		computed,
-		onBeforeUnmount,
-		onMounted,
-		ref,
-		useRuntimeConfig,
-	} from '#imports'
-	import { apply, setAttr } from '@directus/visual-editing'
-	import { useDirectusPreview } from '../composables/directus'
-	import { Slot } from '../utils'
-	import type { DirectusSchema } from '#build/types/directus'
+import type { PrimaryKey } from '@directus/types'
+import {
+	computed,
+	onBeforeUnmount,
+	onMounted,
+	ref,
+	useRuntimeConfig,
+} from '#imports'
+import { apply, setAttr } from '@directus/visual-editing'
+import { useDirectusPreview } from '../composables/directus'
+import { Slot } from '../utils'
+import type { DirectusSchema } from '#build/types/directus'
 
-	type SingleDirectusCollection = DirectusSchema[T] extends Array<any>
-		? DirectusSchema[T][0]
-		: DirectusSchema[T]
-	type FieldKey = keyof SingleDirectusCollection
+type SingleDirectusCollection = DirectusSchema[T] extends Array<any>
+	? DirectusSchema[T][0]
+	: DirectusSchema[T]
+type FieldKey = keyof SingleDirectusCollection
 
-	const props = defineProps<{
-		collection: T
-		item: PrimaryKey
-		fields?: FieldKey | FieldKey[]
-		mode?: 'drawer' | 'modal' | 'popover'
-	}>()
+const props = defineProps<{
+	collection: T
+	item: PrimaryKey
+	fields?: FieldKey | FieldKey[]
+	mode?: 'drawer' | 'modal' | 'popover'
+}>()
 
-	const config = useRuntimeConfig()
-	const directusPreview = useDirectusPreview()
-	const editorElement = ref<HTMLElement | null>(null)
+const config = useRuntimeConfig()
+const directusPreview = useDirectusPreview()
+const editorElement = ref<HTMLElement | null>(null)
 
-	const directusAttr = computed(() => {
-		const data: Record<any, any> = {}
+const directusAttr = computed(() => {
+	const data: Record<any, any> = {}
 
-		Object.entries(props).forEach(([key, value]) => {
-			if (value !== undefined) {
-				data[key] = value
-			}
-		})
-
-		return setAttr(data as any)
-	})
-
-	const attributes = computed(() => {
-		if (!directusPreview.value) {
-			return null
-		}
-
-		return {
-			'data-directus': directusAttr.value,
+	Object.entries(props).forEach(([key, value]) => {
+		if (value !== undefined) {
+			data[key] = value
 		}
 	})
 
-	onMounted(async () => {
-		if (
-			!config.public.directus.visualEditor ||
-			!editorElement.value ||
-			import.meta.server
-		) {
-			return
-		}
+ return setAttr(data as any)
+})
 
-		const applied = await apply({ directusUrl: config.public.directus.url })
+const attributes = computed(() => {
+	if (!directusPreview.value) {
+		return null
+	}
 
-		if (!applied) {
-			return
-		}
+	return {
+		'data-directus': directusAttr.value,
+	}
+})
 
-		applied.enable()
+onMounted(async () => {
+	if (
+		!config.public.directus.visualEditor ||
+		!editorElement.value ||
+		import.meta.server
+	) {
+		return
+	}
 
-		onBeforeUnmount(() => {
-			applied.remove()
-		})
+	const applied = await apply({ directusUrl: config.public.directus.url })
+
+	if (!applied) {
+		return
+	}
+
+	applied.enable()
+
+	onBeforeUnmount(() => {
+		applied.remove()
 	})
+})
 </script>
 
 <template>
