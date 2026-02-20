a3f431dfc9cbd6278b871d32fcacd5010000138b
Kevin Heiner <kheiner@gmail.com>
2026-02-19 21:00:59 -0800

    fix: provide consistent redirect experience for login() and loginWithProvider() Fixes #15

--- playground/pages/auth/login.vue
+++ playground/pages/auth/login.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { reactive, useDirectusAuth } from '#imports'
+import { reactive, readProviders, useAsyncData, useDirectus, useDirectusAuth } from '#imports'
 
-const { user, login, loggedIn } = useDirectusAuth()
+const { user, login, loginWithProvider, loggedIn } = useDirectusAuth()
+const directus = useDirectus()
 
 const form = reactive({
   email: 'admin@example.com',
@@ -11,39 +12,54 @@ const form = reactive({
 async function loginForm() {
   await login(form.email, form.password)
 }
+
+async function providerLogin(provider: string) {
+  await loginWithProvider(provider)
+}
+
+const { data: ssoProviders } = await useAsyncData('ssoProviders', () =>
+  directus.request(readProviders()))
 </script>
 
 <template>
   <div>
-    <form
-      v-if="!loggedIn"
-      @submit.prevent="loginForm"
-    >
-      <div>
-        <label for="email-input">Email</label>
-        <input
-          id="email-input"
-          v-model="form.email"
-          type="email"
-          autocomplete="email"
-          required
-        >
+    <div v-if="!loggedIn">
+      <h1>Login with default provider (username and password)</h1>
+      <form
+        v-if="!loggedIn"
+        @submit.prevent="loginForm"
+      >
+        <div>
+          <label for="email-input">Email</label>
+          <input
+            id="email-input"
+            v-model="form.email"
+            type="email"
+            autocomplete="email"
+            required
+          >
+        </div>
+
+        <div>
+          <label for="password-input">Password</label>
+          <input
+            id="password-input"
+            v-model="form.password"
+            type="password"
+            autocomplete="current-password"
+            required
+          >
+        </div>
+
+        <button>Submit</button>
+      </form>
+      <h1>Login with Providers</h1>
+      <div v-for="provider in ssoProviders" :key="provider.name">
+        <button @click="providerLogin(provider.name)">
+          Log in with {{ provider.name }}
+        </button>
       </div>
-
-      <div>
-        <label for="password-input">Password</label>
-        <input
-          id="password-input"
-          v-model="form.password"
-          type="password"
-          autocomplete="current-password"
-          required
-        >
-      </div>
-
-      <button>Submit</button>
-    </form>
-
+    </div>
     <div v-else>
       <p>You're logged in as:</p>
       <pre>{{ user }}</pre>
